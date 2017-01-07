66ae152a38df548ca93c9e3274a3d462bbfbec63
Joseph Sikorski <joedski@gmail.com>
2017-01-07 16:11:08 -0500

    Ensured that no ref keys are left on a condition, not even default ones, and added JSDoc to Referents EvaluationContextProvider.

--- src/providers/referents.js
+++ src/providers/referents.js
@@ -29,6 +29,27 @@ export const defaultConfig = {
 };
 
 
+/**
+ * Creates an EvaluationContextProvider which converts certain Condition Properties into Referents.
+ * This provides a way to access parts of the app state within your Predicates, which would otherwise
+ * be rather boring or else have to reach for state in a variety of messy ways.
+ * @param  {ReferentsConfig} _config
+ *         The configuration for the created Referents EvaluationContextProvider.
+ * @param  {[string=refs]} nameInContext
+ *         The property name used to attach the generated referents to the enhannced EvaluatioContextLayer.
+ * @param  {{[referentKey: string]: ( appContext: mixed ) => mixed}} defaults
+ *         Default referent selectors to always add to the referents generated.
+ *         They will be added to every EvaluationContextLayer regardless of whether the
+ *         Condition in that Layer has any Referent Keys on it or not.
+ *         However, adding a Referent Key to a Condition will override the value selected
+ *         by the Default Selector.
+ * @param  {{[referentKey: string]: ( appContext: mixed, parameter: mixed ) => mixed}} selectors
+ *         Referent Selectors to use whenever a given referentKey is encountered on a Condition.
+ *         Once a Condition has matched a Selector, all Sub-Conditions under that Condition will
+ *         also have that value for that Referent Key.  That is, Sub-Conditions inherit the
+ *         EvaluationContextLayer of their parent Conditions.
+ * @return {EvaluationContextProvider} The provider to use with your ConditionEvaluator.
+ */
 export default ( _config: ReferentsConfig ): EvaluationContextProvider => {
 	const config: ReferentsConfig = { ...defaultConfig, ..._config };
 	const { defaults, nameInContext, selectors } = config;
@@ -50,8 +71,15 @@ export default ( _config: ReferentsConfig ): EvaluationContextProvider => {
 			const refsTypesOnCondition = referentTypes.filter( refType => refType in condition );
 
 			refsTypesOnCondition.forEach( refType => {
+				// We don't check `refType in condition` here because we just did that
+				// when getting refTypesOnCondition.
 				nextRefs[ refType ] = selectors[ refType ]( appContext, condition[ refType ] );
 				delete condition[ refType ];
+			});
+
+			// Clear off any lingering ref keys if they're ones that only show up in the defaults.
+			defaultReferentTypes.forEach( refType => {
+				if( refType in condition ) delete condition[ refType ];
 			});
 
 			return {
